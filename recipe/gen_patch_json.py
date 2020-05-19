0a83db67eb1d379cc4dca722615e5f12b2c07867
Matthew R Becker <beckermr@users.noreply.github.com>
2020-05-19 09:47:02 -0500

    Update recipe/gen_patch_json.py

--- recipe/gen_patch_json.py
+++ recipe/gen_patch_json.py
@@ -285,7 +285,6 @@ OSX_SDK_FIXES = {
 
 
 def _add_removals(instructions, subdir):
-    global REMOVALS
     r = requests.get(
         "https://conda-static.anaconda.org/conda-forge/"
         "label/broken/%s/repodata.json" % subdir
