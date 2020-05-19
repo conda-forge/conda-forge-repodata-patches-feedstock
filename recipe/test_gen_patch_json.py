7d2c32ac20ba52e5a8680d64023aa169928189e2
Matthew R Becker <beckermr@users.noreply.github.com>
2020-05-19 09:46:11 -0500

    Update recipe/test_gen_patch_json.py

--- recipe/test_gen_patch_json.py
+++ recipe/test_gen_patch_json.py
@@ -26,7 +26,7 @@ def test_gen_patch_instructions():
         'a': {'depends': ['c', 'd', 'e'], 'features': None},
         'c': {'addthis': 'yes'}}
 
-    assert set(REMOVALS['osx-64']) in set(inst['remove'])
+    assert set(REMOVALS['osx-64']) <= set(inst['remove'])
 
 
 def test_add_python_abi():
@@ -96,4 +96,3 @@ def test_add_python_abi():
     }
     add_python_abi(exact_record, "osx-64")
     assert exact_record["constrains"] == []
-
