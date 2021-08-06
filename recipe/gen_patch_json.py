5ab71705c5b4ec97acd2d3c2ae8e76a1af9dab0d
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2021-08-06 22:17:01 +0300

    Patch snowflake-sqlalchemy 1.3.1 (#160)
    * patch sqlalchemy and snowflake-connector-python for snowflake-sqlalchemy 1.3.1 build=0
    
    * clean up
    
    * also patch 1.2.5

--- recipe/gen_patch_json.py
+++ recipe/gen_patch_json.py
@@ -1061,6 +1061,12 @@ def _gen_new_index(repodata, subdir):
                 i = record["depends"].index("py-tools-ds >=0.15.10")
                 record["depends"][i] = "py-tools-ds >=0.15.10,<=0.15.11"
 
+        # https://github.com/conda-forge/conda-forge-repodata-patches-feedstock/issues/159
+        if record_name == "snowflake-sqlalchemy" and record["version"] in ("1.3.1", "1.2.5") and record["build_number"] == 0:
+            depends = record["depends"]
+            depends[depends.index("snowflake-connector-python <3")] = "snowflake-connector-python <3.0.0"
+            depends[depends.index("sqlalchemy <2")] = "sqlalchemy >=1.4.0,<2.0.0"
+
     return index
 
 
