6fdd602fafb1a6bec0818c99a5ab2334a0ff24a6
Matthew R. Becker <beckermr@users.noreply.github.com>
2022-03-04 09:16:07 -0600

    Update gen_patch_json.py

--- recipe/gen_patch_json.py
+++ recipe/gen_patch_json.py
@@ -855,9 +855,7 @@ def _gen_new_index_per_key(repodata, subdir, index_key):
             _pin_looser(fn, record, "openmpi", upper_bound="5.0")
         if any(dep.startswith("mpich >=3.3") for dep in deps):
             _pin_looser(fn, record, "mpich", upper_bound="5.0")
-        if any(dep.startswith("mpich >=3.4,") for dep in deps):
-            _pin_looser(fn, record, "mpich", upper_bound="5.0")
-        if any(dep.startswith("mpich >=3.4.1,") for dep in deps):
+        if any(dep.startswith("mpich >=3.4") for dep in deps):
             _pin_looser(fn, record, "mpich", upper_bound="5.0")
 
         # TBB 2021 (oneTBB 2021) is incompatible with previous releases.
