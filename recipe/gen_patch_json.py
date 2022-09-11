fe7fbddc44741902739163e4573625e220c503be
Isuru Fernando <isuruf@gmail.com>
2022-09-10 21:32:10 -0500

    don't have an upper bound of 3 for libxml2

--- recipe/gen_patch_json.py
+++ recipe/gen_patch_json.py
@@ -826,7 +826,7 @@ def _gen_new_index_per_key(repodata, subdir, index_key):
             if any(dep.startswith("pango >=5.2") for dep in deps):
                 _pin_looser(fn, record, "xz", max_pin="x")
             if any(dep.startswith("libxml2 >=2.9") for dep in deps):
-                _pin_looser(fn, record, "libxml2", max_pin="x")
+                _pin_looser(fn, record, "libxml2", upper_bound="2.11.0")
 
         if any(dep.startswith("expat >=2.2.") for dep in deps) or \
                 any(dep.startswith("expat >=2.3.") for dep in deps):
