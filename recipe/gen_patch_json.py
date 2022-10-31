e66febc3001ccf66610d4e1cc48abaf7e87d42ec
Leo Fang <leo80042@gmail.com>
2022-10-31 12:52:23 -0400

    Numba is not impacted by the ABI breaking

--- recipe/gen_patch_json.py
+++ recipe/gen_patch_json.py
@@ -808,7 +808,7 @@ def _gen_new_index_per_key(repodata, subdir, index_key):
         if i >= 0:
             deps[i] = "cudatoolkit >=11.2,<12.0a0"
 
-        # cuda-python 11.7.1 is ABI-breaking, currently it impacts rmm & numba
+        # cuda-python 11.7.1 is ABI-breaking, currently it impacts only rmm
         if record_name == "rmm":
             if pkg_resources.parse_version(record["version"]) < pkg_resources.parse_version("22.08"):
                 # these were built with cuda-python <= 11.7.0
@@ -817,12 +817,6 @@ def _gen_new_index_per_key(repodata, subdir, index_key):
                 and record['build_number'] == 0):
                 # this is built with cuda-python 11.7.1
                 _replace_pin("cuda-python >=11.7,<12.0a0", "cuda-python >=11.7.1,<12.0a0", deps, record)
-        if (record_name == "numba"
-            and pkg_resources.parse_version(record["version"]) <= pkg_resources.parse_version("0.56.2")):
-            for i, dep in enumerate(record.get("constrains", [])):
-                if dep.startswith("cuda-python"):
-                    record["constrains"][i] = "cuda-python >=11.6,<11.7.1"
-                    break
 
         if record.get('timestamp', 0) < 1663795137000:
             if any(dep.startswith("arpack >=3.7") for dep in deps):
