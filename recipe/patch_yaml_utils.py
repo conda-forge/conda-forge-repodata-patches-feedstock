3ffb760de63c556c6a9188a7cf1b67e4ff31c7b9
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-08-30 05:46:08 -0500

    Update patch_yaml_utils.py

--- recipe/patch_yaml_utils.py
+++ recipe/patch_yaml_utils.py
@@ -7,10 +7,11 @@ import fnmatch as _fnmatch
 import re
 from functools import lru_cache
 
-from patch_yaml_model import PatchYaml, _IfClause, _ThenClauseItem
-
-
+# fmt: off
 ALLOWED_TEMPLATE_KEYS = ["name", "version", "build_number", "subdir"]
+
+from patch_yaml_model import PatchYaml, _IfClause, _ThenClauseItem
+# fmt: on
 
 OPERATORS = ["==", ">=", "<=", ">", "<", "!="]
 
