0870c739d4948d43ec6da47111af53faa21dad30
jaimergp <jaimergp@users.noreply.github.com>
2024-07-18 17:46:55 +0200

    Add repodata patch for rattler-build-conda-compat

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -34,9 +34,9 @@ CONDARC
 export CONDA_LIBMAMBA_SOLVER_NO_CHANNELS_FROM_INSTALLED=1
 
 mamba install --update-specs --yes --quiet --channel conda-forge --strict-channel-priority \
-    pip mamba conda-build conda-forge-ci-setup=4 "conda-build>=24.1"
+    pip mamba conda-build conda-forge-ci-setup=4 "conda-build>=24.1" "rattler-build-conda-compat!=0.1.2"
 mamba update --update-specs --yes --quiet --channel conda-forge --strict-channel-priority \
-    pip mamba conda-build conda-forge-ci-setup=4 "conda-build>=24.1"
+    pip mamba conda-build conda-forge-ci-setup=4 "conda-build>=24.1" "rattler-build-conda-compat!=0.1.2"
 
 # set up the condarc
 setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
